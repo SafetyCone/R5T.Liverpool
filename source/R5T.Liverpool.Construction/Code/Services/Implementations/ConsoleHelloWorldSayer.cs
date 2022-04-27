9427c6c89b82fcb8bb2bcabc93d1cc83df10f5ae
davidcoats <david@getsafetycone.com>
2022-04-27 15:32:34 -0700

    Interim changes.

--- source/R5T.Liverpool.Construction/Code/Services/Implementations/ConsoleHelloWorldSayer.cs
+++ source/R5T.Liverpool.Construction/Code/Services/Implementations/ConsoleHelloWorldSayer.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Liverpool.Construction
-{
-    public class ConsoleHelloWorldSayer : IHelloWorldSayer
+{[ServiceImplementationMarker]
+    public class ConsoleHelloWorldSayer : IHelloWorldSayer,IServiceImplementation
     {
         public void SayHelloWorld()
         {
